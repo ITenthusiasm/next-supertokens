13edc96a7584b02f0d3c5179d5c17bcdcd8a8149
Isaiah Thomason <47364027+ITenthusiasm@users.noreply.github.com>
2024-12-01 18:20:14 -0600

    feat: Add client-side validation to ResetPassword Page (App Dir)

--- templates/app-router/app/reset-password/page.tsx
+++ templates/app-router/app/reset-password/page.tsx
@@ -2,9 +2,11 @@
 
 // Primary Imports
 import Link from "next/link";
-import { use, useActionState } from "react";
-import { sendEmail, resetPassword } from "./actions";
+import { use, useActionState, useState, useEffect, useMemo, startTransition } from "react";
+import { createFormValidityObserver } from "@form-observer/react";
+import { getServerActionProps } from "@/lib/utils/nextisms";
 import { commonRoutes } from "@/lib/utils/constants";
+import { sendEmail, resetPassword } from "./actions";
 
 // Styles
 import "@/app/auth-form.scss";
@@ -20,8 +22,38 @@ interface PageProps {
 export default function ResetPassword(props: PageProps) {
   const searchParams = use(props.searchParams);
   const { mode, token } = searchParams;
+
   const [sendEmailErrors, sendEmailAction] = useActionState(sendEmail, {});
   const [resetPasswordErrors, resetPasswordAction] = useActionState(resetPassword, {});
+
+  // Keep server/client errors in sync
+  const [errors, setErrors] = useState({ ...sendEmailErrors, ...resetPasswordErrors });
+  useEffect(() => setErrors(sendEmailErrors), [sendEmailErrors]);
+  useEffect(() => setErrors(resetPasswordErrors), [resetPasswordErrors]);
+
+  // Manage form errors.
+  const required = (field: HTMLInputElement) => `${field.labels?.[0].textContent} is required`;
+  const { autoObserve, configure, validateField, validateFields } = useMemo(() => {
+    return createFormValidityObserver("focusout", {
+      renderByDefault: true,
+      renderer(errorContainer, errorMessage) {
+        const fieldName = errorContainer.id.replace(/-error$/, "");
+        setErrors((e) => ({ ...e, [fieldName]: errorMessage }));
+      },
+    });
+  }, []);
+
+  const formRef = useMemo(autoObserve, [autoObserve]);
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    if (!validateFields({ focus: true })) return;
+
+    const formData = new FormData(event.currentTarget);
+    startTransition(() => {
+      if (token) return resetPasswordAction(formData);
+      return sendEmailAction(formData);
+    });
+  }
 
   if (mode === "success") {
     return (
@@ -40,40 +72,54 @@ export default function ResetPassword(props: PageProps) {
   if (token) {
     return (
       <main>
-        <form ref={onFormMount} action={resetPasswordAction}>
+        <form ref={formRef} {...getServerActionProps(resetPasswordAction)} onSubmit={handleSubmit}>
           <h1>Change your password</h1>
           <h2>Enter a new password below to change your password</h2>
-          {resetPasswordErrors.banner && <div role="alert">{resetPasswordErrors.banner}</div>}
+          {errors.banner && <div role="alert">{errors.banner}</div>}
 
           <label htmlFor="password">New password</label>
           <input
             id="password"
-            name="password"
             type="password"
             placeholder="New password"
-            pattern="(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}"
-            required
-            aria-invalid={!!resetPasswordErrors.password}
+            aria-invalid={!!errors.password}
             aria-describedby="password-error"
+            {...configure("password", {
+              required,
+              pattern: {
+                value: "(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}",
+                message: "Password must contain at least 8 characters, including a number",
+              },
+              validate(input: HTMLInputElement) {
+                const confirmPassword = input.form?.elements.namedItem("confirm-password") as HTMLInputElement;
+                if (confirmPassword.value) validateField(confirmPassword.name);
+              },
+            })}
           />
           <div id="password-error" role="alert">
-            {resetPasswordErrors.password}
+            {errors.password}
           </div>
 
           <label htmlFor="confirm-password">Confirm password</label>
           <input
             id="confirm-password"
-            name="confirm-password"
             type="password"
             placeholder="Confirm your password"
-            required
-            aria-invalid={!!resetPasswordErrors["confirm-password"]}
+            aria-invalid={!!errors["confirm-password"]}
             aria-describedby="confirm-password-error"
+            {...configure("confirm-password", {
+              required,
+              validate(input: HTMLInputElement) {
+                const password = input.form?.elements.namedItem("password") as HTMLInputElement;
+                if (input.value !== password.value) return "Confirmation password doesn't match";
+              },
+            })}
           />
           <div id="confirm-password-error" role="alert">
-            {resetPasswordErrors["confirm-password"]}
+            {errors["confirm-password"]}
           </div>
 
+          <input name="token" type="hidden" value={token} />
           <button type="submit">CHANGE PASSWORD</button>
         </form>
       </main>
@@ -92,29 +138,23 @@ export default function ResetPassword(props: PageProps) {
 
   return (
     <main>
-      <form ref={onFormMount} action={sendEmailAction}>
+      <form ref={formRef} {...getServerActionProps(sendEmailAction)} onSubmit={handleSubmit}>
         <h1>Reset your password</h1>
         <h2>We will send you an email to reset your password</h2>
 
         <label htmlFor="email">Email</label>
         <input
           id="email"
-          name="email"
-          type="email"
-          required
-          aria-invalid={!!sendEmailErrors.email}
+          aria-invalid={!!errors.email}
           aria-describedby="email-error"
+          {...configure("email", { required, type: { value: "email", message: "Email is invalid" } })}
         />
         <div id="email-error" role="alert">
-          {sendEmailErrors.email}
+          {errors.email}
         </div>
 
         <button type="submit">Email me</button>
       </form>
     </main>
   );
-}
-
-function onFormMount(ref: HTMLFormElement | null): void {
-  if (ref) ref.noValidate = true;
 }
